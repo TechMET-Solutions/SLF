2ac37ec97a17a807bc148edd759f43b84b1cdd7a
sumitpathak914 <sumitpathakofficial914@gmail.com>
2025-10-09 14:18:32 +0530

    changethe date formate

--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -8,10 +8,6 @@ export const formatIndianDate = (dateString) => {
         day: "2-digit",
         month: "2-digit",
         year: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: false,
         timeZone: "Asia/Kolkata", // India Standard Time
     };
 
